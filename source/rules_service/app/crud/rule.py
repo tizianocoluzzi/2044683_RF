a95b552c162da389849547adcaca08880f7b4a1c
Alessandro Arellano Altuna <alessandro-arellano-altuna@arellantium.homenet.telecomitalia.it>
2026-03-09 14:52:38 +0100

    add endpoint to modify and delete rules

--- source/rules_service/app/crud/rule.py
+++ source/rules_service/app/crud/rule.py
@@ -29,3 +29,25 @@ def disable_rules_by_actuator(db: Session, actuator_name: str):
         db.commit()
 
     return rules_to_disable
+
+def update_rule(db: Session, rule_id: int, rule_data: dict):
+    #cerca le regole
+    db_rule = db.query(RuleDB).filter(RuleDB.id == rule_id).first()
+    if db_rule:
+        #aggiorna i campi con i nuovi valori
+        for key, value in rule_data.items():
+            setattr(db_rule, key, value)
+        db.commit()
+        db.refresh(db_rule)
+    return db_rule
+
+def delete_rule(db: Session, rule_id: int):
+    db_rule = db.query(RuleDB).filter(RuleDB.id == rule_id).first()
+    if db_rule:
+        db.delete(db_rule)
+        db.commit()
+    return db_rule
+
+
+
+
